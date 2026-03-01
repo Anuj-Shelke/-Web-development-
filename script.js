8312a89d0077477d31629c81789439ca4bf99307
Anuj Shelke <anujshelke18@gmail.com>
2026-03-01 14:43:48 +0530

    Code to calculate Length of a String

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-//JS Functions 
-function nice(name){
-console.log("Hello "+name+" How are you "); 
-console.log("We are delited to see you "+name); 
-console.log("We know that Advance booking of the name "+name+" Has been done "); 
-console.log("Our Hotel Welcomes "+name+" with Friends "); 
-console.log("Thanks for Visting "+name );
-console.log("We value customers like you "+name);
-}
-
-nice("Anuj");
+let a = "Anuj"; 
+console.log(a.length); 
