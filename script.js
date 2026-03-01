1fc7e357823effa0c35e6df30255a11bad11b2fe
Anuj Shelke <anujshelke18@gmail.com>
2026-03-01 09:33:10 +0530

    program to check if a number is divisible by 2 and 3

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-//Progarm to Demostrate the use of Switch Case 
-let num = 3; 
-let a = num > 4 ? console.log("A is greater than num"): console.log("A is smaller than num "); 
+//Progarm to Check a number is divisible by 2 and 3  
+let num = 6 ; 
+let a =( num % 2 == 0 ) & (num % 3 == 0)  ? console.log("Num is Divisible by 2 and 3 "): console.log("Num is not divisible by 2 and 3 "); 
