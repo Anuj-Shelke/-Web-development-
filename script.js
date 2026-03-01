c0c2e0fe50deadf6ce339528f6add5af1e40ed84
Anuj Shelke <anujshelke18@gmail.com>
2026-03-01 15:13:56 +0530

    String in JavaScript

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let a = "Anuj"; 
 console.log(a.length); 
+console.log(a.startsWith("A"));
+console.log(a.endsWith("J"));
+console.log(a); 
+
+let b = "CODE TO CONVERT GIVEN STRING TO LOWERCASE "; 
+console.log(b.toLowerCase());
+//String in JavaScript is immutable cannot be changed 
