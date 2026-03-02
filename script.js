538fe029d3879b352e8bb4c8bb1dc3a644519edc
Anuj Shelke <anujshelke18@gmail.com>
2026-03-02 20:10:46 +0530

    If else loop in Javascript

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-console.log("Anuj")
-let boxs = document.getElementsByClassName("container")
-console.log(boxs)
-document.getElementById("red").style.backgroundColor = ("red");
-
+let val = 61; 
+function getGrade(val){
+    if(val>90){
+        console.log("A"); 
+    }
+    else if(val>60){
+        console.log("B");
+    }
+    else if(val>35){
+        console.log("C");
+    }
+    else{
+        console.log("Fail");
+    }
+}
+getGrade(val); 
