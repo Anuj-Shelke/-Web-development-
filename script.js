13e2065006de7bb8a29d80f29be00090c93789b5
Anuj Shelke <anujshelke18@gmail.com>
2026-03-03 16:14:35 +0530

    taking input in javascript

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-//code to print even numbers from 1 to 20 
-for(let i = 0 ; i< 21; i++){
-    console.log(i); 
-    i=i+2; 
+let val = prompt("Enter a Number "); 
+for(let i = 0 ; i < val; i++){
+    if(val%2 === 0 ){
+        console.log("The Number is Even "); 
+    }
+    else{
+        console.log("The number is Odd ");
+    }
 }
