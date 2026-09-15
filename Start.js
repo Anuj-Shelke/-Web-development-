196444d29bd88fb1e23136d791efc303c139ec6d
Anuj Shelke <anujshelke18@gmail.com>
2026-09-15 19:28:43 +0530

    Done with CSS Fundamentals

---
+++ Start.js
@@ -0,0 +1,22 @@
+const username= 'Anuj'; 
+console.log(username);
+let age = 21; 
+let skill =['C','Javascript','HTML']; 
+const o={
+    myname : "Anuj", 
+    college : "pvg"
+}; 
+console.log(age);
+console.log(skill); 
+console.log(o);
+console.log(typeof(age)); 
+if(age> 18){
+    console.log("Adult"); 
+}
+else{
+    console.log("Children"); 
+}
+for(let i = 0 ; i <3; i++){
+console.log(skill[i]); 
+}
+const main = document.getElementbyId("var");
